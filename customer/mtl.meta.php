56fd9a56cf91200249b06233ab6c1f82e7581185
multitel <contact@multitel.net>
2014-01-08 16:40:47 -0500

    Update mtl.meta.php
    licensing info

--- customer/mtl.meta.php
+++ customer/mtl.meta.php
@@ -1,3 +1,29 @@
+<?php
+/*
+
+Copyright (c) 2014 Multitel LLC , Multitel Inc ( http://www.multitel.net ) under the MIT License (MIT)
+
+The MIT License (MIT):
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+?>
+
 <link href="http://assets.multitel.net/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
 <link href="http://assets.multitel.net/bootstrap/css/responsive.css" rel="stylesheet" type="text/css">
 <link href="http://assets.multitel.net/fonts/glyphicons/css/glyphicons.css" rel="stylesheet" type="text/css">
