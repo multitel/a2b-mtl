f89ad791784783c652b75cb6016dd7da4ea0c82f
multitel <contact@multitel.net>
2014-01-08 21:40:12 -0500

    Create mtl.smslogs.php

---
+++ customer/mtl.smslogs.php
@@ -0,0 +1,57 @@
+<?php
+/*
+
+Copyright (c) 2014 Multitel LLC , Multitel Inc ( http://www.multitel.net ) under the MIT License (MIT)
+
+The MIT License (MIT):
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+include './lib/customer.defines.php';
+include './lib/customer.module.access.php';
+include './lib/Form/Class.FormHandler.inc.php';
+include './form_data/FG_var_did.inc';
+include './lib/customer.smarty.php';
+include 'mtl.config.php';
+
+if (!has_rights(ACX_DID)) {
+    Header("HTTP/1.0 401 Unauthorized");
+    Header("Location: PP_error.php?c=accessdenied");
+    die();
+}
+
+$HD_Form->setDBHandler(DbConnect());
+$HD_Form->init();
+
+if (!isset ($form_action)) $form_action = "list";
+if (!isset ($action)) $action = $form_action;
+    $HD_Form->create_toppage($form_action);
+
+    // #### HEADER SECTION
+    $smarty->display('main.tpl');
+
+    // #### HELP SECTION
+    if ($form_action == 'list') {
+       // echo $CC_help_list_did;
+    }
+
+
+include('mtl.meta.php');
+include('mtl.sms_logs_v.php');
+?>
